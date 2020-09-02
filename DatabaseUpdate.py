e1efda0cfaa94e0298fc9cfef19c1dbea9ce19c2
nxlwnm <1023950613@qq.com>
2020-09-03 00:48:05 +0800

    Add Update OpenFundDailyTable

--- DatabaseUpdate.py
+++ DatabaseUpdate.py
@@ -10,20 +10,37 @@ from datetime import datetime
 
 class DatabaseUpdate:
     def __init__(self):
-        self.fund_em_fund_name_df = ak.fund_em_fund_name()
         Config.ReadConfig()
 
     def ProceeAction(self):
         with MySQLProxy.MySQLProxy(host=Config.host, user=Config.user, password=Config.password,
                                    database=Config.database, charset=Config.charset) as (conn, cursor):
             self.ProcessAllFundNameTableUpdate(conn, cursor)
+            self.ProcessOpenFundDailyTableUpdate(conn, cursor)
+
+    def ProcessOpenFundDailyTableUpdate(self, conn, cursor):
+        # query if need update
+        sqlSelect: str = '''SELECT * FROM Table_Update_Time WHERE TableName = "Open_Fund_Daily";'''
+        cursor.execute(sqlSelect)
+        results = cursor.fetchall()
+        lastUpdateTime = datetime.min
+        for row in results:
+            if row[0] == "Open_Fund_Daily":
+                lastUpdateTime = row[1]
+
+        # 上次更新时间 与 今天23：00比较/上次更新时间 与 当前时间比较
+        if lastUpdateTime.hour < 23 < datetime.now().hour or lastUpdateTime == datetime.min:
+            self.UpdateOpenFundDailyTable(conn, cursor)
+            self.UpdateTimeTableForOpenFundDailyTable(conn, cursor)
+        else:
+            print("no need to update Open_Fund_Daily table")
 
     def ProcessAllFundNameTableUpdate(self, conn, cursor):
         # query if need update
         sqlSelect: str = '''SELECT * FROM Table_Update_Time WHERE TableName = "All_Fund_Name";'''
         cursor.execute(sqlSelect)
         results = cursor.fetchall()
-        deltaDay = 0
+        deltaDay = 1000
         for row in results:
             if row[0] == "All_Fund_Name":
                 deltaDay = (datetime.now() - row[1]).days
@@ -32,33 +49,9 @@ class DatabaseUpdate:
             # update All_Fund_Name
             self.UpdateAllFundName(conn, cursor)
             # update Table_Update_Time
-            self.UpdateTimeTable(conn, cursor)
+            self.UpdateTimeTableForAllFundTable(conn, cursor)
         else:
             print("no need to update All_Fund_Name table")
-
-    def UpdateTimeTable(self, conn, cursor):
-        sqlSelect: str = '''SELECT TableName FROM Table_Update_Time WHERE TableName = "All_Fund_Name";'''
-        cursor.execute(sqlSelect)
-        results = cursor.fetchall()
-        results = [item[0] for item in results]
-
-        localtime = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        print(localtime)
-        if "All_Fund_Name" in results:
-            sql = '''
-            UPDATE Table_Update_Time 
-            SET LastUpdateTime = str_to_date('%s', '%%Y-%%m-%%d %%H:%%i:%%S');
-            ''' % localtime
-            cursor.execute(sql)
-        else:
-            sql = '''
-                    INSERT INTO Table_Update_Time 
-                    (TableName, LastUpdateTime)
-                    VALUES
-                    ('All_Fund_Name', str_to_date('%s', '%%Y-%%m-%%d %%H:%%i:%%S'));
-                    ''' % localtime
-            cursor.execute(sql)
-        conn.commit()
 
     def UpdateAllFundName(self, conn, cursor):
         sqlSelect: str = ''' SELECT ID FROM All_Fund_Name;'''
@@ -66,9 +59,10 @@ class DatabaseUpdate:
         results = cursor.fetchall()
         results = [item[0] for item in results]
 
-        for row in self.fund_em_fund_name_df.values:
+        fund_em_fund_name_df = ak.fund_em_fund_name()
+        for row in fund_em_fund_name_df.values:
             if int(row[0]) in results:
-                print("update %s" % row[0])
+                print("update AllFundName %s" % row[0])
                 sql = '''UPDATE All_Fund_Name 
                         SET 
                         ID = %s,
@@ -79,7 +73,7 @@ class DatabaseUpdate:
                         WHERE ID = %s;''' % (row[0], row[1], row[2], row[3], row[4], row[0])
                 cursor.execute(sql)
             else:
-                print("INSERT %s" % row[0])
+                print("INSERT AllFundName %s" % row[0])
                 sql = '''INSERT INTO All_Fund_Name 
                 (ID, PinYin, Name, Type, WholeName )
                 VALUES
@@ -88,10 +82,75 @@ class DatabaseUpdate:
                 cursor.execute(sql)
         conn.commit()
 
+    def UpdateOpenFundDailyTable(self, conn, cursor):
+        sqlSelect: str = ''' SELECT ID FROM Open_Fund_Daily;'''
+        cursor.execute(sqlSelect)
+        results = cursor.fetchall()
+        results = [item[0] for item in results]
 
-DU = DatabaseUpdate()
-DU.ProceeAction()
+        fund_em_open_fund_daily_df = ak.fund_em_open_fund_daily()
+        for row in fund_em_open_fund_daily_df.values:
+            for index in range(len(row)):
+                if row[index] == '':
+                    row[index] = "0"
+            if int(row[0]) in results:
+                print("update Open_Fund_Daily %s" % row[0])
+                sql = '''UPDATE Open_Fund_Daily 
+                        SET 
+                        ID = %s,
+                        Name = '%s',
+                        UnitMoney = %s,
+                        AccuMoney = %s,
+                        PrevUnitMoney = %s,
+                        PrevAccuMoney = %s,
+                        DailyGrowthValue = %s,
+                        DailyGrowthRate = %s,
+                        SubscriptionStatus = '%s',
+                        RedemptionStatus = '%s',
+                        Charge = '%s'
+                        WHERE ID = %s;
+                        ''' % (row[0], row[1], row[2], row[3], row[4], row[5],
+                               row[6], row[7], row[8], row[9], row[10], row[0])
+                cursor.execute(sql)
+            else:
+                print("INSERT Open_Fund_Daily %s" % row[0])
+                sql = '''INSERT INTO Open_Fund_Daily 
+                (ID, Name, UnitMoney, AccuMoney, 
+                PrevUnitMoney, PrevAccuMoney, DailyGrowthValue, DailyGrowthRate,
+                SubscriptionStatus, RedemptionStatus, Charge)
+                VALUES
+                (%s, '%s', %s, %s, %s, %s, %s, %s,'%s', '%s', '%s');
+                ''' % (row[0], row[1], row[2], row[3], row[4], row[5],
+                       row[6], row[7], row[8], row[9], row[10])
+                cursor.execute(sql)
+        conn.commit()
 
+    def UpdateTimeTable(self, conn, cursor, table_name: str):
+        sqlSelect: str = '''SELECT TableName FROM Table_Update_Time WHERE TableName = "%s";''' % table_name
+        cursor.execute(sqlSelect)
+        results = cursor.fetchall()
+        results = [item[0] for item in results]
 
+        localtime = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        if table_name in results:
+            sql = '''
+                    UPDATE Table_Update_Time 
+                    SET LastUpdateTime = str_to_date('%s', '%%Y-%%m-%%d %%H:%%i:%%S');
+                    WHERE TableName = '%s';
+                    ''' % (localtime, table_name)
+            cursor.execute(sql)
+        else:
+            sql = '''
+                INSERT INTO Table_Update_Time 
+                (TableName, LastUpdateTime)
+                VALUES
+                ('%s', str_to_date('%s', '%%Y-%%m-%%d %%H:%%i:%%S'));
+                ''' % (table_name, localtime)
+            cursor.execute(sql)
+        conn.commit()
 
+    def UpdateTimeTableForAllFundTable(self, conn, cursor):
+        self.UpdateTimeTable(conn, cursor, "All_Fund_Name")
 
+    def UpdateTimeTableForOpenFundDailyTable(self, conn, cursor):
+        self.UpdateTimeTable(conn, cursor, "Open_Fund_Daily")
--- Main.py
+++ Main.py
@@ -1,34 +1,40 @@
 import akshare as ak
-import DatabaseSetup
-import DatabaseUpdate
+from DatabaseSetup import DatabaseSetup
 from config import Config
-import MySQLProxy
+from MySQLProxy import MySQLProxy
+from DatabaseUpdate import DatabaseUpdate
 
-# 数据库的初始化 一般只需要跑一次
-# 需要考虑如何自动判定
-# 数据库的常态内容需要支持自动更新，比如每7天自动获取并加入到数据库
-# DS = DatabaseSetup.DatabaseSetup()
-# DS.Setup()
+def main():
+    Config.ReadConfig()
 
-# 净值数据需要每天23:以后更新
-Config.ReadConfig()
-with MySQLProxy.MySQLProxy(host=Config.host, user=Config.user, password=Config.password,
-                           database=Config.database, charset=Config.charset) as (conn, cursor):
-    sql : str = '''SELECT * FROM  All_fund_name'''
-    cursor.execute(sql)
-    result: [] = cursor.fetchall()
-    IDs = [item[0] for item in result]
-    result: [] = [[str(InnerItem) for InnerItem in item] for item in result]
-    resultsDict = {IDs[i]: result[i] for i in range(len(IDs))}
+    # 数据库的初始化 一般只需要跑一次
+    # 跑过Setup都需要补一个Update重新更新数据，所以尽量避免更改数据库结构
+    DS = DatabaseSetup()
+    DS.ProceeAction()
 
-fund_em_open_fund_daily_df = ak.fund_em_open_fund_daily()
-print(" ".join(fund_em_open_fund_daily_df.columns))
-for item in fund_em_open_fund_daily_df.values:
-    if int(item[0]) in IDs:
-        print(" ".join(resultsDict[int(item[0])]))
-    else:
-        pass
+    DU = DatabaseUpdate()
+    DU.ProceeAction()
+
+    """    with MySQLProxy(host=Config.host, user=Config.user, password=Config.password,
+                               database=Config.database, charset=Config.charset) as (conn, cursor):
+        sql : str = '''SELECT * FROM  All_fund_name'''
+        cursor.execute(sql)
+        result: [] = cursor.fetchall()
+        IDs = [item[0] for item in result]
+        result: [] = [[str(InnerItem) for InnerItem in item] for item in result]
+        resultsDict = {IDs[i]: result[i] for i in range(len(IDs))}
+
+    fund_em_open_fund_daily_df = ak.fund_em_open_fund_daily()
+    print(" ".join(fund_em_open_fund_daily_df.columns))
+    for item in fund_em_open_fund_daily_df.values:
+        print(" ".join(item))"""
+
+    """    for item in fund_em_open_fund_daily_df.values:
+        if int(item[0]) in IDs:
+            print(" ".join(resultsDict[int(item[0])]))
+        else:
+            pass"""
 
 
-# if __name__ == '__main__':
-#    pass
+if __name__ == '__main__':
+    main()
